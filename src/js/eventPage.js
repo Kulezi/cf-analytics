81c8aaefa409eed121449588dc5c93d22ae4889d
Paweł Putra <pawelputra0@gmail.com>
2023-12-26 13:56:09 +0100

    format js

--- src/js/eventPage.js
+++ src/js/eventPage.js
@@ -1,14 +1,14 @@
-chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
-    if(request.todo == "appendHTML"){
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.todo == "appendHTML") {
         let xhr = new XMLHttpRequest();
-        xhr.open('GET','src/graph.html',true);
-        xhr.onprogress = function(){
+        xhr.open('GET', 'src/graph.html', true);
+        xhr.onprogress = function () {
             console.log('XHR object on progress');
         }
-        xhr.onload = function(){
+        xhr.onload = function () {
             console.log('XHR object loaded');
             sendResponse({
-                htmlResponse : this.responseText,
+                htmlResponse: this.responseText,
             });
         }
         xhr.send();
